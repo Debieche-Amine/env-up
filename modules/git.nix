a25d330a209ffd7d4e357dc3d8135b535659845d
Amine <Debieche-Amine@users.noreply.github.com>
2026-05-08 17:56:07 +0100

    nixos: (20260508-175540)

--- modules/git.nix
+++ modules/git.nix
@@ -9,7 +9,7 @@
       user.name = "Amine";
       user.email = "Debieche-Amine@users.noreply.github.com";
       signing = {
-        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE7cM18ycWHX4UkDI2rIAZMlhBp4WJ0tTMJVgNSUbrbD";
+        # key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE7cM18ycWHX4UkDI2rIAZMlhBp4WJ0tTMJVgNSUbrbD";
         signByDefault = true;
       };
 
