f02c22027367c1392d10be8a4a59482b807653bf
Amine <amine_debieche@proton.me>
2026-05-08 17:45:13 +0100

    nixos: (20260508-174432)

--- modules/git.nix
+++ modules/git.nix
@@ -7,7 +7,12 @@
     enable = true;
     settings = {
       user.name = "Amine";
-      user.email = "";
+      user.email = "amine_debieche@proton.me";
+      signing = {
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE7cM18ycWHX4UkDI2rIAZMlhBp4WJ0tTMJVgNSUbrbD";
+        signByDefault = true;
+      };
+
       color.ui = true;
       push.default = "simple";
       init.defaultBranch = "main";
